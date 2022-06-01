9300c82d22fee2d2925c8e72aef7e189be8311db
Syed Musab Mohiuddin <98490330+itsmekirx909@users.noreply.github.com>
2022-06-01 11:52:38 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 var input = document.getElementById('input')
 var p = document.getElementById('new')
 var taskCount = document.getElementById('taskCount')
+var dd = document.getElementById('tasks')
+
 
 function addTask(e){
 
 if(input.value !== ''){
-var dd = document.getElementById('tasks')
 
 var el = document.createElement('p')
 
@@ -16,13 +17,13 @@ dd.appendChild(el).appendChild(val)
 
 var delBtn = document.createElement("button");
 delBtn.innerHTML = "DEL";
-delBtn.setAttribute('onclick','del()')
+delBtn.setAttribute('onclick','del(this)')
 el.appendChild(delBtn);
 
 
 var editBtn = document.createElement("button");
 editBtn.innerHTML = "EDIT";
-editBtn.setAttribute('onclick','edit()')
+editBtn.setAttribute('onclick','edit(this)')
 el.appendChild(editBtn)
 
 taskCount.innerHTML = dd.childElementCount
@@ -30,11 +31,33 @@ taskCount.innerHTML = dd.childElementCount
 
 input.value = ''
 
-}else(alert('asdsadada'))
+}else(alert('Enter valid value'))
 
 }
 
 function delAll(e){
     document.getElementById('tasks').innerHTML = ''
+taskCount.innerHTML = dd.childElementCount
 }
+
+function del(e){
+e.parentNode.remove()
+taskCount.innerHTML = dd.childElementCount
+}
+
+function edit(e){
+
+var par = e.parentNode
+var edit = document.createElement('input')
+var editBtn = document.createElement('button')
+
+editBtn.innerHTML = 'Save'
+
+par.appendChild(edit)
+par.appendChild(editBtn)
+
+    console.log(e.parentNode.firstChild)
+}
+
+
 
